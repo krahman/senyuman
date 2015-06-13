5ba9aa3be26feeb3364fb78a13123380ec44a5dd
Khalilur Rahman <khal.rahman@gmail.com>
2015-06-13 16:36:29 +0800

    Added init senyuman converter

---
+++ senyum/DaftarSenyuman.swift
@@ -0,0 +1,35 @@
+//
+//  DaftarSenyuman.swift
+//  senyum
+//
+//  Created by nevda on 13/6/15.
+//  Copyright (c) 2015 com.deehan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DaftarSenyumanViewController : UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var senyumans = ["😀", "😵", "😄", "😈"]
+    
+    override func viewDidLoad() {
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.senyumans.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        
+        cell.textLabel!.text = self.senyumans[indexPath.row]
+        
+        return cell
+    }
+    
+}
