33d76173a99985ffe64bf1187f0ec3392ab5150b
Khalilur Rahman <khal.rahman@gmail.com>
2015-06-13 14:05:39 +0800

    Initial Commit

---
+++ senyum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  senyum
+//
+//  Created by nevda on 13/6/15.
+//  Copyright (c) 2015 com.deehan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
